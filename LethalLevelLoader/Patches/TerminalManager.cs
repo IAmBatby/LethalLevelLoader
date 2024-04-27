e29fd454df1efdef047d0f5839940c544b0a3b7b
SylviBlossom <sylviblossom@gmail.com>
2024-04-26 21:43:03 -0700

    Implement save item corruption prevention

--- LethalLevelLoader/Patches/TerminalManager.cs
+++ LethalLevelLoader/Patches/TerminalManager.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using Unity.Netcode;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
