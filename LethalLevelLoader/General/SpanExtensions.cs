572d68b2d8c95796b25a4b99e7d395178f314020
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-29 09:09:43 +0700

    Create `SpanExtensions`

---
+++ LethalLevelLoader/General/SpanExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LethalLevelLoader.General;
+internal static class SpanExtensions
+{
+    public static bool IsLettersOrDigits(this ReadOnlySpan<char> span)
+    {
+        for (var i = 0; i < span.Length; i++)
+        {
+            if (char.IsLetterOrDigit(span[i]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool IsLetters(this ReadOnlySpan<char> span)
+    {
+        for (var i = 0; i < span.Length; i++)
+        {
+            if (char.IsLetter(span[i]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static ReadOnlySpan<char> TrimStartToLetters(this ReadOnlySpan<char> span)
+    {
+        var startIndex = 0;
+        for (var i = 0; i < span.Length; i++)
+        {
+            if (char.IsLetter(span[i]))
+            {
+                break;
+            }
+
+            startIndex++;
+        }
+
+        return span.Slice(startIndex);
+    }
+}
