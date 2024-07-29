8d3423fb45fffdcc686e0460b59a9545af9c519f
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-29 09:10:59 +0700

    Create `ArrayExtensions`

---
+++ LethalLevelLoader/General/ArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LethalLevelLoader.General;
+internal static class ArrayExtensions
+{
+    public static void AddItem<T>(ref T[] array, T item)
+    {
+        if (array == null)
+        {
+            array = new T[1] { item };
+            return;
+        }
+
+        Array.Resize(ref array, array.Length + 1);
+        array[array.Length - 1] = item;
+    }
+}
